7d00a153c6fa6b4be97ac01fcbe28ffa5314886c
JamesFahey <james.fahey@hotmail.co.uk>
2021-08-15 22:15:24 +0000

    add turn base function to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
 
+const playText = getElementsById('playText');
+const spaces = [null, null, null, null, null, null, null, null, null,];
 const O_TEXT = "O";
-const X_TEXR = "X";
+const X_TEXT = "X";
 let currentPlayer = O_TEXT;
 
 const drawBoard = () => {
@@ -25,8 +27,59 @@ const drawBoard = () => {
 };
 
 const boxClicked = (e) => {
-    console.log('boc clicked');
+    const id = e.target.id;
+    if (!spaces[id]){
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        if(playerHasWon()){
+            playText.innerText = `${currentPlayer} has won!`;
+            return;
+        }
+        currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
+    }
 }
+
+const playerHasWon = () => {
+    //from top left, check across, down, and diagonal
+  if (spaces[0] === player) {
+    if (spaces[1] === player && spaces[2] === player) {
+      console.log(`${player} wins up top`);
+      return true;
+    }
+    if (spaces[3] === player && spaces[6] === player) {
+      console.log(`${player} wins on the left`);
+      return true;
+    }
+    if (spaces[4] === player && spaces[8] === player) {
+      console.log(`${player} wins on the diagonal`);
+      return true;
+    }
+  }
+  //from bottom check up and across
+  if (spaces[8] === player) {
+    if (spaces[2] === player && spaces[5] === player) {
+      console.log(`${player} wins on the right`);
+      return true;
+    }
+    if (spaces[7] === player && spaces[6] === player) {
+      console.log(`${player} wins on the bottom`);
+      return true;
+    }
+  }
+  //from middle check middle vertical and middle horizontal
+  if (spaces[4] === player) {
+    if (spaces[3] === player && spaces[5] === player) {
+      console.log(`${player} wins on the middle horizontal`);
+      return true;
+    }
+    if (spaces[1] === player && spaces[7] === player) {
+      console.log(`${player} wins on the middle vertical`);
+      return true;
+    }
+  }
+};
+
 
 drawBoard();
 
